8c348b05771044a8e5771f6ca2b64230b45c8062
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 22:43:27 +0600

    Add project files.

---
+++ Mod_2_17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mod_2_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            bool isEven = n % 2 == 0;
+            if (isEven)
+            {
+                Console.WriteLine($" Число {n} является четным.");
+            }
+            else
+            {
+                Console.WriteLine($" Число {n} не является четным.");
+            }
+
+
+            bool end7 = n % 10 == 7;
+            if (end7)
+            {
+                Console.WriteLine($" Число {n} оканчивается цифрой 7.");
+            }
+            else
+            {
+                Console.WriteLine($" Число {n} не оканчивается цифрой 7.");
+            }
+        }
+    }
+}
